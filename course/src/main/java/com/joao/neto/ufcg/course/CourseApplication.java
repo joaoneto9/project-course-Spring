4e6d31d1ee40d6da985b76d2515d096adf7ea67b
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2025-04-22 16:08:35 -0300

    feat: adicionando o Spring boot e rodando ele no rpojeto

---
+++ course/src/main/java/com/joao/neto/ufcg/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.joao.neto.ufcg.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ course/src/test/java/com/joao/neto/ufcg/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.neto.ufcg.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
