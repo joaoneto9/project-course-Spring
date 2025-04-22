3bf25a89c5fa9d6e5c3d968b9cc3826cb6605bb9
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2025-04-22 16:49:18 -0300

    feat: assinaturas de RestController, GetMapping e RequestMapping atribuidos no Resource do Users. implementacaco da Requisicao Get de uma lista de Objetos User em Json

---
+++ course/src/main/java/com/joao/neto/ufcg/course/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.joao.neto.ufcg.course.resources;
+
+import com.joao.neto.ufcg.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users") // mapeia, nomeiando o Local
+public class UserResource {
+
+    @GetMapping // responde a requisicao GET http.
+    public List<ResponseEntity<User>> findAllUsers() {
+        List<ResponseEntity<User>> lista = new ArrayList<>();
+        User u = new User(1, "joao", "joao@gmail", "99999", "1234");
+        lista.add(ResponseEntity.ok().body(u));
+        lista.add(ResponseEntity.ok().body(new User(2, "barbara", "barabara@gmail.com", "98900809", "2222")));
+        return lista; // retorna o Body desse User "u"
+    }
+}
